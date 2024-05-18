03d07ec8bf2bdb0dfa073087eda5e0654108a206
jjuny-won <jjuny040627@gamil.com>
2024-05-18 13:45:45 +0900

    [FEAT]:카카오 로그인 수정

--- src/main/java/com/example/toex/user/domain/params/KakaoInfoResponse.java
+++ src/main/java/com/example/toex/user/domain/params/KakaoInfoResponse.java
@@ -1,23 +1,38 @@
 package com.example.toex.user.domain.params;
 
-import lombok.Getter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
-@Getter
-public class KakaoInfoResponse {
+@Data
+public class KakaoInfoResponse implements UserInfo {
+    @JsonProperty("id")
     private Long id;
-    private Properties properties;
-    private KakaoAccount kakao_account;
 
-    @Getter
-    @ToString
+    @JsonProperty("kakao_account")
+    private KakaoAccount kakaoAccount;
+
+    @Data
     public static class KakaoAccount {
+        @JsonProperty("email")
         private String email;
+
+        @JsonProperty("profile")
+        private Profile profile;
+
+        @Data
+        public static class Profile {
+            @JsonProperty("nickname")
+            private String nickname;
+        }
     }
 
-    @Getter
-    @ToString
-    public static class Properties {
-        private String nickname;
+    @Override
+    public String getEmail() {
+        return kakaoAccount.getEmail();
+    }
+
+    @Override
+    public String getName() {
+        return kakaoAccount.getProfile().getNickname();
     }
 }
